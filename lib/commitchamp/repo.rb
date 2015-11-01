06d29ee23bea1a0d91e6b86cc17dd92a60718440
Sherri Mitchell <rsamitchell@att.net>
2015-11-01 18:58:41 -0500

    Added methods to get repo info and stats. Added prompt.

--- lib/commitchamp/repo.rb
+++ lib/commitchamp/repo.rb
@@ -4,4 +4,35 @@ module Commitchamp
     has_many :contributions
     has_many :users, through: :contributions
     end
+
+    def get_repos(repo_name)
+      repo = Repo.first_or_create(name: repo_name)
+      results = @github.get_repo_contributors(owner, repo_name)
+      results.each do |contributor|
+        user = User.first_or_create(name: contributor['author']['login'])
+    end
+
+
+
+
+    private
+    def get_repo(org, repo)
+      contribution["author"]["login"]
+    end
+
+    def get_stats(contribution, stat)
+      weeks = contribution["weeks"]
+      weeks.inject(0) { |sum, item| sum + item[stat] }
+    end
+
+    def prompt(message, validator)
+      puts message
+      input = gets.chomp
+      until input =~ validator
+        puts "I'm sorry. I didn't understand that answer."
+        puts message
+        input = gets.chomp
+      end
+      input
+    end
   end
